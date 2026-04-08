ec2d6ca50ea512ac7c06e61001e8cab2cb7795a2
shivani4161sharma-design <shivani4161sharma@gmail.com>
2026-04-08 13:28:38 +0530

    Final foolproof Vercel configuration

--- hospital/urls.py
+++ hospital/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('doctors.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
